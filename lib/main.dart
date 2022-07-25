0d2bcee8aa5bd0061d15e617ab7ec42848bfcecd
FlutLab.io <>
2022-07-25 15:11:25 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
         Expanded(child: Image(image: AssetImage('images/dice1.png'))),
         Expanded(child: Image(image: AssetImage('images/dice1.png')))
