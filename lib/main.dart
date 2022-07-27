bcddcfdbede3318ffb365cb33c62e44925620d54
FlutLab.io <>
2022-07-27 08:23:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 5;
   int rightDiceNumber = 2;
+  void chnageDiceFcae() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,20 +41,14 @@ class _DicePageState extends State<DicePage> {
         Expanded(
             child: FlatButton(
           onPressed: () => {
-            setState(() {
-              leftDiceNumber = Random().nextInt(6) + 1;
-              rightDiceNumber = Random().nextInt(6) + 1;
-            }),
+            chnageDiceFcae(),
           },
           child: Image.asset('images/dice$leftDiceNumber.png'),
         )),
         Expanded(
             child: FlatButton(
           onPressed: () => {
-            setState(() {
-              rightDiceNumber = Random().nextInt(6) + 1;
-              leftDiceNumber = Random().nextInt(6) + 1;
-            }),
+            chnageDiceFcae(),
           },
           child: Image.asset('images/dice$rightDiceNumber.png'),
         )),
